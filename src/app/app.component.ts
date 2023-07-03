d366bf51feb6d738e5caa3363ba63f0e3a41b442
btbnazmul <btbnazmula@barytech.com>
2023-07-03 19:04:15 +0600

    translator added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'amgularForm';
+  title = 'angularForm';
+
+  defaultCountry = 'Bangladesh';
+
+  firstname!:string;
+  lastname!:string;
+  email!: string;
+  gen!:string;
+  country!: string;
+
+  defaultGender = 'Male';
+
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ]
+  
+  @ViewChild('myForm') form!: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+
+    this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'smith';
+    // this.form.value.personDetails.email = 'abc@example.com';
+    // this.form.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@example.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+         firstname: 'John',
+         lastname: 'Smith',
+         email: 'abc@example.com',
+      }
+    })
+  }
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'de', 'fr', 'hi']);
+    translate.setDefaultLang('en');
+    // translate.use('hi');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang!.match(/en|de/) ? browserLang !: 'en');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +23,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CheckboxModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
